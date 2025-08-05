120a1152c7cf2a07575733d3a31d20fa28ea2295
joaov-sha <joaovitor8832@gmail.com>
2025-08-05 16:53:39 -0300

    Adicionados cliques aos botões da interface para adicionar amigos à lista e sortear amigo

--- challenge-amigo-secreto/src/components/main/Main.tsx
+++ challenge-amigo-secreto/src/components/main/Main.tsx
@@ -4,20 +4,35 @@ import {useState} from 'react';
 function Main(){
     const [nome, setNome] = useState("");
     const [lista, setLista] = useState<string[]>([]);
+    const [sorteado, setSorteado] = useState<string | null>(null);
 
     function addToList(){
         const name = nome.trim();
         if(!name){
             alert("Por favor, insira um nome para continuar!");
             return
-        }else{
-            setLista([...lista, name]);
-            setNome("");
         }
+
+        if(lista.includes(name)){
+            alert("Nome já adicionado!");
+            return;
+        }
+
+        setLista([...lista, name]);
+        setNome("");
     }
 
     function drawFriend(){
-        
+        if(lista.length === 0){
+            alert("Não há nomes para sortear!");
+            return;
+        }
+
+        const position = Math.floor(Math.random() * lista.length);
+        const nomeSorteado = lista[position];
+
+        setSorteado(nomeSorteado);
+        setLista(lista.filter(nome => nome !== nomeSorteado));
     }
 
     return(
@@ -36,8 +51,13 @@ function Main(){
                     </ul>
                 </div>
                 <div className={styles.controls}>
-                    <button><span className="material-symbols-outlined">play_circle</span>Sortear amigo</button>
+                    <button onClick={drawFriend}><span className="material-symbols-outlined">play_circle</span>Sortear amigo</button>
                 </div>
+                {sorteado && (
+                    <div className={styles.resultado}>
+                        <p><b>Amigo sorteado: {sorteado}</b></p>
+                    </div>
+                )}
             </div>
         </div>
     )
