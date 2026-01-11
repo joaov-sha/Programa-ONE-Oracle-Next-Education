df4bfa36172fb9f7403e6c0926d1747e7598bc2b
joaov-sha <joaovitor8832@gmail.com>
2026-01-11 15:30:24 -0300

    Removidos arquivos criados de forma duplicada ao realizar a criação rápida de arquivos

--- java-e-orientacao-a-objetos-2/quarta-aula/desafio/carro/ModeloCarro.java
+++
@@ -1,9 +0,0 @@
-package carro;
-public class ModeloCarro extends Carro{
-    
-    public ModeloCarro(){}
-
-    public ModeloCarro(String nome, int ano, double preco){
-        super(nome, ano, preco);
-    }
-}
--- java-e-orientacao-a-objetos-2/quarta-aula/desafio/numeros/GeradorPrimo.java
+++
@@ -1,4 +0,0 @@
-package numeros;
-public class GeradorPrimo {
-    
-}
