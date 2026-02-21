90f0a01992720a2090868714b3fa12b2e664cf6c
joaov-sha <joaovitor8832@gmail.com>
2026-02-20 23:53:54 -0300

    Adicionado/modificado o arquivo referente à solução da sexta etapa do desafio da primeira aula do curso

--- java-lambdas-streams-spring-framework/primeira-aula/desafio/contador/src/main/java/org/joaov_sha/contador/model/Avaliacao.java
+++ java-lambdas-streams-spring-framework/primeira-aula/desafio/contador/src/main/java/org/joaov_sha/contador/model/Avaliacao.java
@@ -2,12 +2,38 @@ package org.joaov_sha.contador.model;
 
 public class Avaliacao<T> {
 
+    private T avaliacao;
     private String comentario;
     private double nota;
 
-    public Avaliacao(String comentario, double nota){
+    public Avaliacao(T avaliacao, String comentario, double nota){
+        this.avaliacao = avaliacao;
         this.comentario = comentario;
         this.nota = nota;
     }
     
+    public T getAvaliacao(){
+        return avaliacao;
+    }
+
+    public void setAvaliacao(T avalicao){
+        this.avaliacao = avalicao;
+    }
+
+    public String getComentario(){
+        return comentario;
+    }
+
+    public void setComentario(String comentario){
+        this.comentario = comentario;
+    }
+
+    public double getNota(){
+        return nota;
+    }
+
+    public void setNota(Double nota){
+        this.nota = nota;
+    }
+
 }
