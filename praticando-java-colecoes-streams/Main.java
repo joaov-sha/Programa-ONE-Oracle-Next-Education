198db362ab42220b7bd6876af73646258a632e80
joaov-sha <joaovitor8832@gmail.com>
2026-03-02 17:03:42 -0300

    Removidas importações não utilizadas do código

--- praticando-java-colecoes-streams/Main.java
+++ praticando-java-colecoes-streams/Main.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
